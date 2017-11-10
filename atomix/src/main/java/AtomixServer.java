587f551a78c2dce99f4a9df75bb007303b391c36
soedomoto <soedomoto@gmail.com>
2017-11-10 10:31:31 +0700

    Add atomix server

---
+++ atomix/src/main/java/AtomixServer.java
@@ -0,0 +1,19 @@
+import io.atomix.AtomixReplica;
+import io.atomix.catalyst.transport.Address;
+import io.atomix.catalyst.transport.netty.NettyTransport;
+import io.atomix.copycat.server.storage.Storage;
+
+import java.util.UUID;
+
+public class AtomixServer {
+    public static void main(String[] args) throws Exception {
+        Address address = new Address("184.3.145.57", 12345);
+        AtomixReplica replica = AtomixReplica.builder(address)
+                .withTransport(new NettyTransport())
+                .withStorage(Storage.builder()
+                        .withDirectory(System.getProperty("user.dir") + "/logs/" + UUID.randomUUID().toString())
+                        .build())
+                .build();
+        replica.bootstrap();
+    }
+}
